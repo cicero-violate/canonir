01f42a25e8955d2b4e7fbe4f7cd23659d32a73f0
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-24 23:25:04 -0500

    auto

--- run_orchestration.sh
+++ run_orchestration.sh
@@ -8,3 +8,7 @@ cargo run
 
 ./run_capture.sh test_projects/test_captured_project \
     test_projects/test_captured_project/model_ir_captured.json
+
+cargo run -p orchestration -- \
+  test_projects/test_captured_project/model_ir_captured.json \
+  test_projects/test_captured_project
