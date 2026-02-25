3a22d27fd578b07417403d787da860e153bede37
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-25 18:32:17 -0500

    auto

--- run_orchestration.sh
+++ run_orchestration.sh
@@ -1,12 +1,14 @@
 cargo build --workspace
 
-rm -rf /workspace/ai_sandbox/canon/test_projects/test_rust_project/test_capture/model_ir_captured.json
-rm -rf test_projects/test_rust_project/test_emit/
+
+rm -rf /workspace/ai_sandbox/canon/test_projects/test_rust_project/model_ir_captured.json
+
+rm -rf /workspace/ai_sandbox/canon/test_projects/test_rust_project/test_emit/
 cargo run -p orchestration -- \
   test_projects/test_rust_project/model_ir.json \
   test_projects/test_rust_project/test_emit
 
-rm -rf test_projects/test_rust_project/test_capture/
+rm -rf /workspace/ai_sandbox/canon/test_projects/test_rust_project/test_capture/
 ./run_capture.sh test_projects/test_rust_project/test_emit/ \
     test_projects/test_rust_project/test_capture/model_ir_captured.json
 
@@ -14,5 +16,7 @@ cargo run -p orchestration -- \
   test_projects/test_rust_project/test_capture/model_ir_captured.json \
   test_projects/test_rust_project/test_capture
 
-cd   test_projects/test_rust_project/test_capture
+cd /workspace/ai_sandbox/canon/test_projects/test_rust_project/test_capture
 cargo build
+
+cp /workspace/ai_sandbox/canon/test_projects/test_rust_project/test_capture/model_ir_captured.json /workspace/ai_sandbox/canon/test_projects/test_rust_project/
