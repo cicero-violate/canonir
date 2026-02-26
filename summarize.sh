200dac92402355043c465411ecfd62f5942875be
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-26 16:56:56 -0500

    auto

--- summarize.sh
+++ summarize.sh
@@ -6,5 +6,6 @@ bat -n /workspace/ai_sandbox/canon/ISSUE.md
 bat -n /workspace/ai_sandbox/canon/GOAL.md
 bat -n /workspace/ai_sandbox/canon/PLAN.md
 bat -n /workspace/ai_sandbox/canon/EXECUTION_STATUS.md
+bat -n /workspace/ai_sandbox/canon/EXECUTION_REF.md
 
 tree --gitignore -I 'chatgpt_rustsandbox'
