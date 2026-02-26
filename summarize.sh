d777f0f99950d030aa53d848db2fe713f36b9e48
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-26 14:58:31 -0500

    auto

--- summarize.sh
+++ summarize.sh
@@ -2,8 +2,8 @@ bat -n /workspace/ai_sandbox/canon/PROJECT_OVERVIEW.md
 bat -n /workspace/ai_sandbox/canon/AGENT_RULES.md
 
 bat -n /workspace/ai_sandbox/canon/ISSUE.md
-bat -n /workspace/ai_sandbox/canon/EXECUTION_STATUS.md
 bat -n /workspace/ai_sandbox/canon/GOAL.md
 bat -n /workspace/ai_sandbox/canon/PLAN.md
+bat -n /workspace/ai_sandbox/canon/EXECUTION_STATUS.md
 
 tree --gitignore
