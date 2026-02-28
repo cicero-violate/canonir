7d1f5a7382e4ea71213081c8ee10646894e7316a
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-27 23:47:10 -0500

    auto

---
+++ canon-agent/summarize.sh
@@ -0,0 +1,10 @@
+bat -n PROJECT_OVERVIEW.md
+bat -n GOAL.md
+git log -n 8
+bat -n AGENT_STATE.md
+bat -n PLAN.md
+# bat -n /workspace/ai_sandbox/canon/EXECUTION_STATUS.md
+# bat -n /workspace/ai_sandbox/canon/EXECUTION_REF.md
+# bat -n /workspace/ai_sandbox/canon/AGENT_STATE.md
+
+tree --gitignore -I 'chatgpt_rustsandbox'
