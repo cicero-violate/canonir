88072ed627c592c1f0c12a255306c8ad8fc7e0c3
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-01 22:33:02 -0500

    auto

--- summarize.sh
+++ summarize.sh
@@ -1,7 +1,16 @@
 # git log -n 8
-bat -n /workspace/ai_sandbox/canon/PROJECT_OVERVIEW.md
+# bat -n /workspace/ai_sandbox/canon/PROJECT_OVERVIEW.md
 # bat -n /workspace/ai_sandbox/canon/PROJECT_STATUS.md
-bat -n /workspace/ai_sandbox/canon/AGENT.md
+# bat -n /workspace/ai_sandbox/canon/AGENT.md
+
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/AGENT_ACT.md
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/AGENT_BOOTSTRAP.md
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/agent_config.toml
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/AGENT_GOAL.md
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/AGENT_OBSERVE.md
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/AGENT_PLAN.md
+bat -n /workspace/ai_sandbox/canon/canon-agent-prompts/AGENT_VERIFY.md
+
 
 # bat -n /workspace/ai_sandbox/canon/ISSUE.md
 bat -n /workspace/ai_sandbox/canon/GOAL.md
@@ -12,3 +21,14 @@ bat -n /workspace/ai_sandbox/canon/GOAL.md
 
 tree --gitignore -I 'chatgpt_rustsandbox'
 
+
+bat -n canon-agent/src/pipelines/invariant/observe.rs
+bat -n canon-agent/src/pipelines/invariant/config.rs
+bat -n canon-agent/src/pipelines/invariant/mod.rs
+bat -n canon-agent/src/pipelines/invariant/plan.rs
+bat -n canon-agent-prompts/AGENT_BOOTSTRAP.md
+bat -n canon-agent-prompts/AGENT_OBSERVE.md
+bat -n canon-agent-prompts/AGENT_PLAN.md
+bat -n canon-agent-prompts/AGENT_ACT.md
+bat -n canon-agent-prompts/AGENT_VERIFY.md
+bat -n canon-agent-prompts/agent_config.toml
