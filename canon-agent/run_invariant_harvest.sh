2566cd9dbf24a7deaeeb5829a2eb49af00604f50
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-01 21:50:50 -0500

    auto

--- canon-agent/run_invariant_harvest.sh
+++ canon-agent/run_invariant_harvest.sh
@@ -6,9 +6,3 @@ cargo run -p canon-agent -- run-invariant \
    1
 
 
-# cargo run -p canon-agent -- run-invariant \
-#   /workspace/ai_sandbox/canon \ (THIS IS FUCKING CALLED CWD)
-#   /workspace/ai_sandbox/canon/test_projects/test_rust_projects/capture/repomap \ (THIS IS FUCKIGN CALLED CAPTURE_DIRECTORY)
-#   /workspace/ai_sandbox/canon/test_projects/test_rust_projects/emit/repomap \ (THIS IS FUCKING CALLED EMIT_DIRECTORY)
-#   /workspace/ai_sandbox/canon/target/debug/orchestration \
-#    5
