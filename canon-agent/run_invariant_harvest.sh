68299de49fdf3c96bce4577cbf6bbcc7360973e3
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-03 09:30:44 -0500

    auto

--- canon-agent/run_invariant_harvest.sh
+++ canon-agent/run_invariant_harvest.sh
@@ -1,6 +1,6 @@
-cargo run -p canon-agent -- run-multi-dag \
-  /workspace/ai_sandbox/canon \
-  1000
+# cargo run -p canon-agent -- run-multi-dag \
+#   /workspace/ai_sandbox/canon \
+#   1000
 
 cargo run -p canon-agent -- run-capability \
   /workspace/ai_sandbox/canon 
