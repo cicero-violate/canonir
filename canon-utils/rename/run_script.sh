6fc8f0775eafd5ba56ecd634ca0670c548cc0fbd
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-07 06:32:46 -0500

    auto

--- canon-utils/rename/run_script.sh
+++ canon-utils/rename/run_script.sh
@@ -3,4 +3,5 @@ RUSTC_WRAPPER=/workspace/ai_sandbox/canon/target/debug/canon-span-launcher \
 CARGO_INCREMENTAL=0 \
 rustup run nightly cargo check --manifest-path /workspace/ai_sandbox/canon/canon-agent/Cargo.toml
 
-# RENAME_OFFSET=0 RENAME_LIMIT=5 cargo run --example rename_self
+# RENAME_OFFSET=0 RENAME_LIMIT=5 RENAME_MODE=incremental cargo run --example rename_self
+RENAME_OFFSET=0 RENAME_LIMIT=20 RENAME_MODE=bulk cargo run --example rename_self
