c854325e5d38711819d0d1e9eef4bba20ada7871
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-08 20:09:59 -0400

    auto

--- canon-utils/rename/src/core/mod.rs
+++ canon-utils/rename/src/core/mod.rs
@@ -1,8 +1,14 @@
+// pub mod oracle;
+// pub mod project_editor;
+// pub mod rustc_session;
+// pub mod syn_patcher;
+// pub mod symbol_id;
+
+// pub use oracle::{StructuralEditOracle, StructuralEditOracleApi};
+// pub use project_editor::{ChangeReport, EditConflict, ProjectEditor};
+
 pub mod oracle;
 pub mod project_editor;
 pub mod rustc_session;
-pub mod syn_patcher;
 pub mod symbol_id;
-
-pub use oracle::{StructuralEditOracle, StructuralEditOracleApi};
-pub use project_editor::{ChangeReport, EditConflict, ProjectEditor};
+pub mod syn_patcher;
