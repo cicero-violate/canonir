b53c0a81ed5d20348799a05cb10e84294dbecdfb
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-08 20:11:17 -0400

    auto

--- canon-utils/rename/src/core/mod.rs
+++ canon-utils/rename/src/core/mod.rs
@@ -9,6 +9,13 @@
 
 pub mod oracle;
 pub mod project_editor;
+
+mod project_editor_helpers;
+mod project_editor_load;
+mod project_editor_ops;
+mod project_editor_rewrite;
+mod project_editor_types;
+
 pub mod rustc_session;
 pub mod symbol_id;
 pub mod syn_patcher;
