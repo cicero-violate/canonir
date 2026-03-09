39003f60712f9e019fd922050a87ba7fad388115
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-08 20:08:26 -0400

    auto

--- canon-utils/rename/src/core/project_editor.rs
+++ canon-utils/rename/src/core/project_editor.rs
@@ -1,12 +1,12 @@
-// #[path = "project_editor_helpers.rs"]
+#[path = "project_editor_helpers.rs"]
 mod project_editor_helpers;
-// #[path = "project_editor_load.rs"]
+#[path = "project_editor_load.rs"]
 mod project_editor_load;
-// #[path = "project_editor_ops.rs"]
+#[path = "project_editor_ops.rs"]
 mod project_editor_ops;
-// #[path = "project_editor_rewrite.rs"]
+#[path = "project_editor_rewrite.rs"]
 mod project_editor_rewrite;
-// #[path = "project_editor_types.rs"]
+#[path = "project_editor_types.rs"]
 mod project_editor_types;
 
 pub use project_editor_types::{ChangeReport, EditConflict, ProjectEditor};
