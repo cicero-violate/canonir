f08edcb391c3096aa94e93dc782f8616ab4be9dc
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-10 12:04:45 -0400

    auto

--- canon-agent-v2/run_agent.sh
+++ canon-agent-v2/run_agent.sh
@@ -1,6 +1,6 @@
 # cargo run -p canon-agent -- run-multi-dag \
 #   /workspace/ai_sandbox/canon \
 #   1000
-cargo run -p canon-agent --features cuda -- run-capability \
+cargo run -p canon-agent-v2 --features cuda -- run-capability \
   /workspace/ai_sandbox/canon  
 
