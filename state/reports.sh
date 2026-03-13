7a1d4943f9662ac67a19fe455e3a98791f15b9dc
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-13 03:53:39 -0400

    auto

--- state/reports.sh
+++ state/reports.sh
@@ -5,7 +5,7 @@ cargo clean
 rm -rf kernel_logs/*
 cargo build
 
-CANON_REPORTS_PANIC_ON_CALLSITE_MISMATCH=1 \ # Panics when CALL edges exist but CALL_SITE nodes are 0.
+# CANON_REPORTS_PANIC_ON_CALLSITE_MISMATCH=1 \ # Panics when CALL edges exist but CALL_SITE nodes are 0.
 # CANON_REPORTS_PANIC_ON_BLOCK_MISMATCH=1 \ # Panics when functions exist but HAS_BLOCK or FLOW is 0.
 # CANON_REPORTS_PANIC_ON_NO_BRANCHES=1 \ # Panics when functions exist but no branch nodes (fan-out > 1) are detected.
 # CANON_REPORTS_PANIC_ON_SPARSE_CALLGRAPH=1 \ # Panics when calls_per_function < 0.05.
