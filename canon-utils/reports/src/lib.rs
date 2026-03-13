a71264a9dbf83fd8201a72ac1081fd004933e9e9
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-13 09:38:43 -0400

    auto

--- canon-utils/reports/src/lib.rs
+++ canon-utils/reports/src/lib.rs
@@ -1,18 +1,7 @@
-mod reports;
-mod artifacts_loader;
-pub mod graph;
-pub mod artifacts;
-pub mod analysis;
+pub mod legacy;
 pub mod panic_capture;
-pub mod health;
-pub mod ingest;
-pub mod repair;
-pub mod invariants;
-pub mod semantics;
 pub mod consumer;
-
-pub use reports::generate_reports;
-pub use reports::generate_reports_from_tlog;
-pub use repair::error_surface::{augment_with_errors, write_repair_surface};
+//
+pub use canon_analysis::repair::error_surface::{augment_with_errors, write_repair_surface};
 pub use consumer::ReportEventConsumer;
-pub use invariants::invariant_validator::run_invariant_pipeline;
+pub use canon_analysis::invariants::invariant_validator::run_invariant_pipeline;
