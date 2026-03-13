145e8ed342ca2c095d3615a1e672cdf61fd23f13
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-12 22:06:23 -0400

    auto

--- state/reports.sh
+++ state/reports.sh
@@ -1,7 +1,7 @@
 cargo clean
 rm -rf kernel_logs/*
 cargo build
-cargo run \
+CANON_REPORTS_PANIC_ON_EMPTY_CFG=1 cargo run \
   --manifest-path /workspace/ai_sandbox/canon/canon-utils/reports/Cargo.toml \
   --bin reports_from_tlog \
   -- \
