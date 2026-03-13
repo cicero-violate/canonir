0ff9c3480828703345751ed5ce7e0d96ff37c66e
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-13 09:40:07 -0400

    auto

--- canon-utils/reports/src/lib.rs
+++ canon-utils/reports/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod legacy;
 pub mod panic_capture;
 pub mod consumer;
-//
+
 pub use canon_analysis::repair::error_surface::{augment_with_errors, write_repair_surface};
 pub use consumer::ReportEventConsumer;
 pub use canon_analysis::invariants::invariant_validator::run_invariant_pipeline;
