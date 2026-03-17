8f9452831129aeb52925a49b925e8c2b31409894
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-17 05:59:16 -0400

    auto

--- canon-utils/canon-capability/src/lib.rs
+++ canon-utils/canon-capability/src/lib.rs
@@ -2,10 +2,8 @@ pub mod context;
 pub mod registry;
 pub mod result;
 pub mod r#trait;
-pub mod routing;
-
+//
 pub use context::CapabilityExecutionContext;
 pub use registry::CapabilityRegistry;
 pub use result::CapabilityExecutionResult;
 pub use r#trait::CapabilityHandler;
-pub use routing::route_capability;
