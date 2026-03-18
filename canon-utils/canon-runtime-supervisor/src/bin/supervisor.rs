fd0fcee1377a402c16b488b2e0596727dd5bb601
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-18 18:46:50 -0400

    auto

--- canon-utils/canon-runtime-supervisor/src/bin/supervisor.rs
+++ canon-utils/canon-runtime-supervisor/src/bin/supervisor.rs
@@ -4,5 +4,6 @@ use std::path::Path;
 
 fn main() -> Result<()> {
     let binary = Path::new("/workspace/ai_sandbox/canon/target/debug/canon-runtime");
-    run_binary_supervisor(binary)
+    run_binary_supervisor(binary);
+    Ok())
 }
