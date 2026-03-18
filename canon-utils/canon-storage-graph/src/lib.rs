7548b6c29e65c759d8a7df2a21a098b28307a28b
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-18 16:58:52 -0400

    auto

--- canon-utils/canon-storage-graph/src/lib.rs
+++ canon-utils/canon-storage-graph/src/lib.rs
@@ -4,6 +4,7 @@ pub mod artifacts;
 pub mod health;
 pub mod ingest;
 pub mod consumer;
+pub mod goal_graph_projector;
 
 pub use artifacts_loader::{CodeGraph, Node as GraphNode, Edge as GraphEdge, CsrGraph};
 pub use canon_event_store::{CodeGraphEdge, CodeGraphNode};
