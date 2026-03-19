035abfeea1e1d6309cee7641d414902ebf1c2db0
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-19 01:03:32 -0400

    auto

--- canon-utils/canon-tools-editor/src/fs.rs
+++ canon-utils/canon-tools-editor/src/fs.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
-
+//
 pub fn collect_rs_files(root: &Path) -> Result<Vec<PathBuf>> {
     let mut files = Vec::new();
     for entry in WalkDir::new(root).into_iter().filter_map(Result::ok) {
