2993339d4127e7ce5b38ff18a26ced3b187ac9c5
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-27 19:46:24 -0400

    auto

--- harness_agent_test.sh
+++ harness_agent_test.sh
@@ -1,3 +1,5 @@
+cargo build --bin canon-runtime --bin canon-runtime-supervisor --bin canon-harness-repair
+
 scripts/run_harness_repair.sh \
   canon-route \
   policy::tests::apply_route_policy_forces_plan_when_validation_is_precondition_blocked \
