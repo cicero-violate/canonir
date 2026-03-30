e67dd854f56799f11959266dd204878078860462
Cicero Bugash <cicero.bugash@gmail.com>
2026-03-30 11:05:51 -0400

    auto

--- canon-utils/canon-mini-agent/run.sh
+++ canon-utils/canon-mini-agent/run.sh
@@ -2,6 +2,7 @@ cargo run --bin canon-mini-agent -- --executor
 cargo run --bin canon-mini-agent -- --verifier
 
 cargo run -p canon-mini-agent --bin canon-mini-agent -- --orchestrate --start intent
+cargo run -p canon-mini-agent --bin canon-mini-agent -- --orchestrate --start planner
 cargo run -p canon-mini-agent --bin canon-mini-agent -- --orchestrate --start executor
 cargo run -p canon-mini-agent --bin canon-mini-agent -- --orchestrate --start verifier
-cargo run -p canon-mini-agent --bin canon-mini-agent -- --orchestrate --start planner
+
