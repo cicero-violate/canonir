b6d26732c31f156907bbec2001d50ccf35c039d1
Cicero Bugash <cicero.bugash@gmail.com>
2026-04-02 22:34:28 -0400

    auto

--- canon-utils/canon-mini-agent/run.sh
+++ canon-utils/canon-mini-agent/run.sh
@@ -1,1 +1,9 @@
-done
+
+/workspace/ai_sandbox/canon/target/debug/canon-mini-agent --orchestrate --instance agent_0 --port 9103
+
+/workspace/ai_sandbox/canon/target/debug/canon-mini-agent --orchestrate --instance agent_1 --port 9104
+
+/workspace/ai_sandbox/canon/target/debug/canon-mini-agent --orchestrate --instance agent_2 --port 9105
+
+/workspace/ai_sandbox/canon/target/debug/canon-mini-agent --orchestrate --instance agent_3 --port 9106
+
